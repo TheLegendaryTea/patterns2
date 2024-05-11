9b54a876506900c79bb00fd7dce62d33220f39ae
Fremy <dp8446732@gmail.com>
2024-05-11 15:41:34 +0200

    Добавьте файлы проекта.

---
+++ patterns2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class ProductSubsystem
+{
+    public void CheckAvailability(string productName)
+    {
+        Console.WriteLine($"Проверка наличия товара {productName}");
+    }
+
+    public void PlaceOrder(string productName, int quantity)
+    {
+        Console.WriteLine($"Заказ товара {productName} в количестве {quantity}");
+    }
+}
+
+class DeliverySubsystem
+{
+    public void ScheduleDelivery(string productName, int quantity, string address)
+    {
+        Console.WriteLine($"Запланирована доставка {quantity} шт. товара {productName} по адресу {address}");
+    }
+}
+
+class OrderFacade
+{
+    private ProductSubsystem productSubsystem;
+    private DeliverySubsystem deliverySubsystem;
+
+    public OrderFacade()
+    {
+        productSubsystem = new ProductSubsystem();
+        deliverySubsystem = new DeliverySubsystem();
+    }
+
+    public void PlaceOrder(string productName, int quantity, string address)
+    {
+        productSubsystem.CheckAvailability(productName);
+        productSubsystem.PlaceOrder(productName, quantity);
+        deliverySubsystem.ScheduleDelivery(productName, quantity, address);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        OrderFacade orderFacade = new OrderFacade();
+
+        orderFacade.PlaceOrder("Книга", 2, "ул. Пушкина, д. 10");
+    }
+}
